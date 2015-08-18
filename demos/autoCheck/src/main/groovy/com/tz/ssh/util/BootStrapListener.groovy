a5b0c112dae41d2fd74b837e9173e4a2c8986f85
usedtolove <usedtolove@163.com>
2015-08-19 00:23:45 +0800

    add sit tomcat

--- demos/autoCheck/src/main/groovy/com/tz/ssh/util/BootStrapListener.groovy
+++ demos/autoCheck/src/main/groovy/com/tz/ssh/util/BootStrapListener.groovy
@@ -3,12 +3,18 @@ package com.tz.ssh.util
 import javax.servlet.ServletContextEvent
 import javax.servlet.ServletContextListener
 import javax.servlet.annotation.WebListener
+import java.sql.Driver
+import java.sql.DriverManager
+import java.sql.SQLException
 
 /**
+ * 项目 BootStrap 监听器
+ * 启动时初始化 path 变量，
+ * 关闭时释放数据库驱动
  * Created by hjl on 2015/8/15.
  */
 @WebListener
-class MyPathListener implements ServletContextListener {
+class BootStrapListener implements ServletContextListener {
 
     @Override
     void contextInitialized(ServletContextEvent servletContextEvent) {
@@ -18,6 +24,12 @@ class MyPathListener implements ServletContextListener {
 
     @Override
     void contextDestroyed(ServletContextEvent servletContextEvent) {
-
+        try {
+            for(driver in DriverManager.drivers){
+                DriverManager.deregisterDriver(driver);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
