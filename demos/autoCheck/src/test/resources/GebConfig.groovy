66ab48e0ab6a0e85137198bc9ceba73504447cd4
Julian Hu <usedtolove@gmail.com>
2015-08-19 12:16:48 +0800

    update gradle config file

--- demos/autoCheck/src/test/resources/GebConfig.groovy
+++ demos/autoCheck/src/test/resources/GebConfig.groovy
@@ -2,7 +2,7 @@
 	This is the Geb configuration file.
 	See: http://www.gebish.org/manual/current/configuration.html
 */
-
+import org.apache.tools.ant.taskdefs.condition.Os
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxProfile
 import org.openqa.selenium.firefox.internal.ProfilesIni
@@ -12,7 +12,13 @@ waiting {
 }
 
 // default is to use firefox
-FirefoxProfile fxProfile = new ProfilesIni().getProfile("default");
-driver = { new FirefoxDriver(fxProfile) }
+if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+	println "Windows OS";
+	FirefoxProfile fxProfile = new ProfilesIni().getProfile("default");
+	driver = { new FirefoxDriver(fxProfile) }
+}else{
+	println "Other OS"
+	driver = { new FirefoxDriver() }
+}
 
 baseUrl = "http://localhost:18888/autoCheck/"
